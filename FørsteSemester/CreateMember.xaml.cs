175d52eb7ed9c7aead4eb9d62d8d3eb8004f179f
Ludvig24 <lute5@student.eadania.dk>
2025-12-01 09:17:39 +0100

    Mandag 1/12 -25

--- FørsteSemester/CreateMember.xaml.cs
+++ FørsteSemester/CreateMember.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace FørsteSemester
-{
+{ 
     /// <summary>
     /// Interaction logic for CreateMember.xaml
     /// </summary>
