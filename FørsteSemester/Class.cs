36fb98d0715c4428630d178784ca0b68e9f5dd17
Ludvig24 <lute5@student.eadania.dk>
2025-12-08 09:56:37 +0100

    Classes, så alt er kopiret fra Tobias' branch

--- FørsteSemester/Class.cs
+++ FørsteSemester/Class.cs
@@ -11,7 +11,7 @@ namespace FørsteSemester
 {
     class Class
     {
-        //Attributter til klassen Hold
+        //attributter til Hold
         private string activity;
         private string className;
         private int classID;
@@ -22,11 +22,12 @@ namespace FørsteSemester
         private byte requiredMinAge;
         private byte requiredMaxAge;
         private string memberIDsInClass;
-         
+
+
         //Metoden AddMemberIDToClass, som tilføje et medlems ID til et specifikt hold 
         public void AddMemberIDToClass(int UserID, int classID)
         {
-            string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //sti til documents mappen
             string filepath = Path.Combine(dir, "GitHub\\SemesterEt\\FørsteSemester\\Classes.txt"); //sti til Classes.txt fil
 
             classID.ToString();
@@ -40,7 +41,7 @@ namespace FørsteSemester
 
             //opretter en liste over class Id'er via GetClassData metoden
             List<string> ClassIDs = Admin.GetClassData(8);
-          
+
 
             using (StreamWriter streamWriter = new StreamWriter(filepath)) //har ikke skrevet true - medfører at append er false - vi overskriver filen
             {
@@ -51,28 +52,21 @@ namespace FørsteSemester
                 for (int i = 0; i < ClassIDs.Count; i++)
                 {
                     //if statement der tjekker hvornår det class id i ClassIDs på index i er lig med classID 
-                    if (classID.ToString() == ClassIDs[i]) 
+                    if (classID.ToString() == ClassIDs[i])
                     {
                         lineNumber = i; //sætter lineNumber lig med i for at få den linje i lines hvor id'et classID er
                     }
                 }
 
-                //gemmer informationerne om den class på linjen "lineNumber" i et string array. Split kaldes for at splitte stringen op ved hvert semicolon 
-                string[] currentClass = lines[lineNumber].Split(";");
-                //opretter en int og gemmer værdien på index 4 (som svarer til joinedAmount) i currentClass arrayet. Convert.ToInt32 kaldes for at konverterer fra string til int
-                int CurrentJoinAmount = Convert.ToInt32(currentClass[4]);
-                //tæller CurrentJoinAmount op med 1
-                CurrentJoinAmount++;
-                // tildeler index 4 i currentClass den nye værdi af CurrentJoinAmount
-                currentClass[4] = CurrentJoinAmount.ToString();
-                //kalder string.Join og kombinerer hvert element i currentClass arrayet med et semicolon og gemmer det i en string.
-                string classStringJoined = string.Join(";", currentClass); 
-                //Erstatter elementet på pladsen "lineNumber" i arrayet lines med den nye string classStringJoined
-                lines[lineNumber] = classStringJoined;
 
-                
-                // tilføjer stringen på pladsen "linenumber" i lines arrayet stringen MemberIDClass
-                lines[lineNumber] = lines[lineNumber] + MemberIDClass;
+                string[] currentClass = lines[lineNumber].Split(";"); //gemmer informationerne om den class på linjen "lineNumber" i et string array. Split kaldes for at splitte stringen op ved hvert 
+                int CurrentJoinAmount = Convert.ToInt32(currentClass[4]); //opretter en int og gemmer værdien på index 4 (som svarer til joinedAmount) i currentClass arrayet. Convert.ToInt32 kaldes for at konverterer fra string til 
+                CurrentJoinAmount++;  //tæller CurrentJoinAmount op med 
+                currentClass[4] = CurrentJoinAmount.ToString();  // tildeler index 4 i currentClass den nye værdi af 
+                string classStringJoined = string.Join(";", currentClass);  //kalder string.Join og kombinerer hvert element i currentClass arrayet med et semicolon og gemmer det i en string
+                lines[lineNumber] = classStringJoined; //Erstatter elementet på pladsen "lineNumber" i arrayet lines med den nye string classStringJoined
+                lines[lineNumber] = lines[lineNumber] + MemberIDClass; // tilføjer stringen på pladsen "linenumber" i lines arrayet stringen MemberIDClass
+
                 //for loop der skriver arrayet lines ned i text filen Classes.txt. loopet kører så længe tællervariablen i er mindre end længden på lines arrayet
                 //for hver iteration skrives stringen på index "i" i lines arrayet ned i filen Classes.txt
                 for (int i = 0; i < lines.Length; i++)
@@ -83,8 +77,13 @@ namespace FørsteSemester
 
             }
         }
-    
-        //Get og set, så man kan kalde på følgende privat Fields/Attributter i andre klasser
+
+        //Get Set metoder
+        public void getStatus()
+        {
+
+        }
+
         public string GetActivity()
         {
             return activity;
@@ -181,7 +180,7 @@ namespace FørsteSemester
 
         public void SetMemberIDsInClass(string[] memberIDsInClass)
         {
-            
+
             for (int i = 9; i < memberIDsInClass.Length; i++) //starter fra index 9 for at undgå de første attributter i klassen
             {
                 this.memberIDsInClass = this.memberIDsInClass + memberIDsInClass[i];
