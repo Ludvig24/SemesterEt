36e70f1560825e4eaf6c1500da561642f41eb591
Ludvig24 <lute5@student.eadania.dk>
2025-12-09 14:19:58 +0100

    JoinClass wpf kommenteret

--- FørsteSemester/JoinClass.xaml.cs
+++ FørsteSemester/JoinClass.xaml.cs
@@ -79,117 +79,124 @@ namespace FørsteSemester
                     ledigePladser.Text = $"Ledige pladser: {Classes[i].GetAvailableSpots() - Classes[i].GetJoinedAmount()}"; // Der beregnes ledige pladser ved at trække joinedAmount fra availableSpots
 
                 // nedeunder tilføjes alle TextBlock elementerne til stackpanelet
-                stackPanel.Children.Add(classNameText);
+                    stackPanel.Children.Add(classNameText);
                     stackPanel.Children.Add(classActivityText);
                     stackPanel.Children.Add(ClassGenderText);
                     stackPanel.Children.Add(AlderText);
                     stackPanel.Children.Add(ledigePladser);
 
-
+                //Her laves en if sætning der tjekker om medlemmet allerede er tilmeldt holdet
+                //Hvis medlemmet er tilmeldt holdet, tilføjes et tomt og usynligt item i listen i stedet for holdet
                 if (member.GetJoinedClasses().Contains(Classes[i].GetClassID()))
                 {
-                    ListBoxItem tomitem = new ListBoxItem();
-                    ClassesList.Items.Add(tomitem);
-                    tomitem.IsEnabled = false;
-                    tomitem.Visibility = Visibility.Hidden;
+                    ListBoxItem tomitem = new ListBoxItem(); //Opretter et tomt ListBoxItem
+                    ClassesList.Items.Add(tomitem); //Tilføjer det tomme item til listen
+                    tomitem.IsEnabled = false; //Deaktiverer itemet så det ikke kan vælges
+                    tomitem.Visibility = Visibility.Hidden; //Gør itemet usynligt
                 }
                 else
                 {
-                    if (Classes[i].GetStatus() == true)
+                    if (Classes[i].GetStatus() == true) //Tjekker om holdet er fuldt
                     {
                         //Tilføjer item til listen med holdet
-                        item.BorderBrush = Brushes.Red;
-                        ClassesList.Items.Add(item);
+                        item.BorderBrush = Brushes.Red; //Sætter border farven til rød for at indikere at holdet er fuldt
+                        ClassesList.Items.Add(item); //Tilføjer item til listen som nu er gjort rødt
                     }
                     else
                     {
-                        ClassesList.Items.Add(item);
+                        ClassesList.Items.Add(item); //Hvis holdet ikke er fuldt tilføjes item til listen som normalt
                     }
                 }
                 
             }
         }
+        //Metode der håndterer klik på tilmeld knappen
         private void Tilmeld_Click(object sender, RoutedEventArgs e)
         {
-            
+            // Henter listen af hold, som medlemmet kan tilmelde sig
             List<Class> Teams = member.LoadTeams();
-            Class team = new Class();
-            int classID = ClassesList.SelectedIndex +1; //Skal ændres hvis vi kunne tænke os at ændre rækkefølgen
-            
-            int j = 0;
-            while (j < Teams.Count)
-            {
-                if (classID == Teams[j].GetClassID())
-                {
-                    team = Teams[j];
-                }
-                j++;
-            }
+            Class team = new Class(); // Opretter et tomt Class objekt til at holde det valgte hold
+            int classID = ClassesList.SelectedIndex +1; // Får classID baseret på det valgte indeks i listen (tilføjer 1 fordi indeks starter ved 0)
 
+            //Det som denne while loop gør er at sammenkople det valgte indeks med det hold der er i hold listen med alle hold.
+            int j = 0; //Laver en tæller j til at iterere gennem holdene
+            while (j < Teams.Count) //Loop der går igennem alle holdene
+            {
+                if (classID == Teams[j].GetClassID()) //Tjekker om det valgte classID matcher holdets ID 
+                {
+                    team = Teams[j]; //Hvis der er et match, sættes team objektet til det valgte hold
+                }
+                j++; //Tælleren øges for at gå til næste hold
+            }
+            //Tjekker om holdet er fuldt
             if (team.GetAvailableSpots() == team.GetJoinedAmount())
             {
-                    team.SetStatus(true);
+                //Hvis holdet er fuldt, sættes status til true og der vises en besked til brugeren
+                team.SetStatus(true);
                     MessageBox.Show("Der er desværre ikke flere ledige pladser på dette hold.", "Fyldt Hold");
                 return;
             }
+            //Hvis holdet ikke er fuldt, fortsætter processen
             else
             {
                 
             }
 
-                char GetRequiredGenderInChar = team.GetRequiredGender();
-           
-            if (team.GetRequiredGender() == member.GetGender() || team.GetRequiredGender() == 'B')
+            //laver en variabel der henter det krævede køn for holdet
+            char GetRequiredGenderInChar = team.GetRequiredGender();
+            //Tjekker om medlemmet opfylder kønskravet for holdet
+            if (team.GetRequiredGender() == member.GetGender() || team.GetRequiredGender() == 'B') //Hvis ens køn er det samme som holdets køn, eller hvis holdets køn er begge køn gå den videre
             {
-               
-            } else
+                //Hvis kønskravet er opfyldt, fortsætter processen
+            }
+            else
             {
+                //Hvis kønskravet ikke er opfyldt, vises en besked til brugeren
                 MessageBox.Show("Du opfylder ikke kønskravet for dette hold.", "Uopfyldt Krav");
                 return;
             }
 
-            if (member.GetAge() < team.GetRequiredMinAge() || member.GetAge() > team.GetRequiredMaxAge())
+            if (member.GetAge() < team.GetRequiredMinAge() || member.GetAge() > team.GetRequiredMaxAge()) //Tjekker om medlemmet opfylder alderskravet for holdet
             {
+                //Hvis alderskravet ikke er opfyldt, vises en besked til brugeren
                 MessageBox.Show("Du opfylder ikke alderskravet for dette hold.", "Uopfyldt krav");
                 return;
             }
             else
             {
-                
+                //Hvis alderskravet er opfyldt, fortsætter processen
             }
 
-                member.JoinClass(classID, member.GetUserID());
-            
+            // Kalder JoinClass metoden på member objektet for at tilmelde medlemmet til holdet. Gå til JoinClass definition for mere info
+            member.JoinClass(classID, member.GetUserID());//Her tilføjes medlemmet til holdet i Members.txt filen
+
             int i = 0;
-            while (i < Teams.Count)
+            while (i < Teams.Count) //Loop der går igennem alle holdene
             {
-                if (classID == Teams[i].GetClassID())
+                if (classID == Teams[i].GetClassID()) //Tjekker om det valgte classID matcher holdets ID
                 {
-                    Teams[i].AddMemberIDToClass(member.GetUserID(), classID);
-                    Teams[i].SetJoinedAmount(Teams[i].GetJoinedAmount() + 1);
+                    //Hvis der er et match, tilføjes medlemmet til holdet og joinedAmount øges med 1
+                    Teams[i].AddMemberIDToClass(member.GetUserID(), classID);//Her tilføjes medlemmet til holdet i Classes.txt filen
+                    Teams[i].SetJoinedAmount(Teams[i].GetJoinedAmount() + 1); //Øger joinedAmount med 1
                 }
                 i++;
             }
-            // så for at blokkere den box efter tilmelding, så man ikke tilmelder sig det samme hold flere gange
-            ListBoxItem tomitem = new ListBoxItem();
-            ListBoxItem listBoxItem = (ListBoxItem)ClassesList.SelectedItem;
-            listBoxItem = tomitem;
-            ClassesList.Items.Insert(ClassesList.SelectedIndex,listBoxItem);
-            ClassesList.Items.RemoveAt(ClassesList.SelectedIndex);
-            tomitem.IsEnabled = false;
-            tomitem.Visibility = Visibility.Hidden;
+            //  Her blokkere vi den box efter tilmelding, så man ikke tilmelder sig det samme hold flere gange
+            ListBoxItem tomitem = new ListBoxItem(); //Opretter et tomt ListBoxItem
+            ListBoxItem listBoxItem = (ListBoxItem)ClassesList.SelectedItem; //Henter det valgte item i listen
+            listBoxItem = tomitem; //Erstatter det valgte item med et tomt item
+            ClassesList.Items.Insert(ClassesList.SelectedIndex,listBoxItem);//Indsætter det tomme item på den valgte position
+            ClassesList.Items.RemoveAt(ClassesList.SelectedIndex); //Fjerner det oprindelige item
+            tomitem.IsEnabled = false; //Deaktiverer det tomme item
+            tomitem.Visibility = Visibility.Hidden; //Gør det tomme item usynligt
 
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        
+        private void Tilbageknap_Click(object sender, RoutedEventArgs e) // Metode der sender en tilbage til tidligere vindue
         {
-            
-        }
-
-        private void Tilbageknap_Click(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-            window.Show();
+            this.Hide(); // Skjuler det nuværende vindue
+            window.Show(); // Viser det tidligere vindue
         }
     }
 }
