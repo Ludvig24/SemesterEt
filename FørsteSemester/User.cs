c34e972af7b3be4fddcdd31c5c1cbb092aa9c40d
rain6633 <ept0706@gmail.com>
2025-12-09 14:16:06 +0100

    Update User.cs

--- FørsteSemester/User.cs
+++ FørsteSemester/User.cs
@@ -28,15 +28,12 @@ namespace FørsteSemester
         //metode til at loade alle hold fra tekstfilen og gemme dem i en liste af Class objekter
         public List<Class> LoadTeams()
         {
-            string[] lines = System.IO.File.ReadAllLines(filepath);
-            List<Class> teamsList = new List<Class>();
             string[] lines = System.IO.File.ReadAllLines(filepath); //array der indeholder alle linjer i tekstfilen
             List<Class> teamsList = new List<Class>();//opretter en liste til at gemme Class objekterne
 
 
             for (int i = 0; i < lines.Count(); i++)
             {
-                string teamData = lines[i];
                 string teamData = lines[i]; //laver array indeks i om til en string variabel
                 Class Teams = new Class();
                 string[] Teamsplit = teamData.Split(";"); //splitter ved ; for at tilføje dataen til et array
@@ -54,7 +51,6 @@ namespace FørsteSemester
 
 
 
-                teamsList.Add(Teams);
                 teamsList.Add(Teams); //tilføjer det oprettede Class objekt til listen
             }
 
