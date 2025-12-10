91c9e8789d503ba277dbaca1adb1cb197749aea8
Ludvig24 <lute5@student.eadania.dk>
2025-12-10 09:34:21 +0100

    Onsdag Ludvig har kommenteret wpf kode

--- FørsteSemester/CreateClass.xaml.cs
+++ FørsteSemester/CreateClass.xaml.cs
@@ -20,12 +20,15 @@ namespace FørsteSemester
     /// </summary>
     public partial class CreateClass : Window
     {
+        // Her opretter vi et admin objekt som kan bruges i hele klassen
         Admin admin;
 
+        // Konstruktor for CreateClass klassen, som tager et Admin objekt som parameter
         internal CreateClass(Admin admin)
         {
+            // Her opretter vi komponenterne i vinduet. og så gemmer vi admin som vi fik fra tidligere vindue til admin der er i dettevindue
             this.admin = admin;
-            InitializeComponent();
+            InitializeComponent(); // Initialiserer komponenterne i vinduet
         }
 
         //Opretter et hold baseret på input fra tekstboksene når knappen "OpretHold" bliver klikket
@@ -44,18 +47,18 @@ namespace FørsteSemester
             }
 
             //Kalder CreateClass metoden fra Admin klassen med de værdier der er indtastet i tekstboksene
-            admin.CreateClass(AktivitetBox.Text, HoldNavnBox.Text, Convert.ToByte(PladsBox.Text), gender ,Convert.ToByte(AlderMaxBox.Text), Convert.ToByte(AlderMinBox.Text));
+            admin.CreateClass(AktivitetBox.Text, HoldNavnBox.Text, Convert.ToByte(PladsBox.Text), gender ,Convert.ToByte(AlderMaxBox.Text), Convert.ToByte(AlderMinBox.Text)); //Opretter holdet
             MessageBox.Show("Hold oprettet!"); //Besked der viser at holdet er oprettet
             this.Close();
             Adminmenu adminmenu = new Adminmenu(admin);
             adminmenu.Show();
         }
 
-        //Går tilbage til Adminmenu vinduet når "TilbageKnap" bliver klikket
+        //Går tilbage(gemmer nuværende vindue og laver en ny adminvindue) til Adminmenu vinduet når "TilbageKnap" bliver klikket
         private void TilbageKnap_Click(object sender, RoutedEventArgs e)
         {
             this.Hide();
-            Adminmenu adminmenu = new Adminmenu(admin);
+            Adminmenu adminmenu = new Adminmenu(admin); //Opretter et nyt Adminmenu vindue
             adminmenu.Show();
         }
     }
--- FørsteSemester/Mainmenu.xaml.cs
+++ FørsteSemester/Mainmenu.xaml.cs
@@ -19,38 +19,39 @@ namespace FørsteSemester
     /// </summary>
     public partial class Mainmenu : Window
     {
+        //Her opretter vi to objekter som kan bruges i hele klassen
         Window window;
         Member member;
 
-        internal Mainmenu(Window window, Member member)
+        internal Mainmenu(Window window, Member member) //Constructor til Mainmenu som tager et Window objekt og et Member objekt som parametre
         {
             InitializeComponent();
-            this.window = window;
-            this.member = member;
-            
+            this.window = window; //Tildeler window objekt fra constructor til det window attribut i klassen
+            this.member = member;//Tildeler member objekt fra constructor til den member attribut i klassen
+
         }
 
         private void Logud_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
-            window.Show();
+            this.Close(); //lukker Mainmenu vinduet
+            window.Show(); //viser login vinduet
         }
 
         private void Tilmeld_Click(object sender, RoutedEventArgs e) 
         {
-            JoinClass joinClass = new JoinClass(this, member);
-            joinClass.Show();
-            this.Hide();
+            JoinClass joinClass = new JoinClass(this, member); //Opretter et JoinClass vindue og sender Mainmenu vinduet og Member objektet som parametre
+            joinClass.Show(); //viser JoinClass vinduet
+            this.Hide(); //gemmer Mainmenu vinduet
 
-           
+
         }
 
         private void MineHold_Click(object sender, RoutedEventArgs e)
         {
-            ClassOverview classOverview = new ClassOverview(this, member);
-            classOverview.Show();
+            ClassOverview classOverview = new ClassOverview(this, member); //Opretter et ClassOverview vindue og sender Mainmenu vinduet og Member objektet som parametre
+            classOverview.Show(); //viser ClassOverview vinduet
 
-                this.Hide();
+            this.Hide(); //gemmer Mainmenu vinduet
         }
     }
     
