36ec01b73fe58791091328fd9bebf05b16e264f1
Laura Marie Balle Jensen <laje11@student.eadania.dk>
2025-12-10 17:17:03 +0100

    Kommentar ændringer i Class

--- FørsteSemester/Class.cs
+++ FørsteSemester/Class.cs
@@ -11,7 +11,7 @@ namespace FørsteSemester
 {
     class Class
     {
-        //attributter til Hold
+        //Attributter til Hold
         private string activity;
         private string className;
         private int classID;
@@ -27,47 +27,41 @@ namespace FørsteSemester
         //Metoden AddMemberIDToClass, som tilføje et medlems ID til et specifikt hold 
         public void AddMemberIDToClass(int UserID, int classID)
         {
-            string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //sti til documents mappen
-            string filepath = Path.Combine(dir, "GitHub\\SemesterEt\\FørsteSemester\\Classes.txt"); //sti til Classes.txt fil
+            //Statiske variabler til at finde stien til tekstfilen, hvor hold data bliver gemt. Kombinere mappen "Documents" med stien til Classes.txt
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //Finder stien til mappen "Documents" som er ens for alle computere
+            string filepath = Path.Combine(dir, "GitHub\\SemesterEt\\FørsteSemester\\Classes.txt"); // Kombinerer stien til mappen "Documents" med stien til Classes.txt filen
 
             classID.ToString();
 
-            string MemberIDClass = "0"; //starter med ID 0 for at undgå fejl ved første tilføjelse
-
-            MemberIDClass = ";" + UserID; //tilføjer et semicolon før ID'et for at matche formatet i tekstfilen
-
-            //læser alle linjer i tekstfilen Classes.txt og gemmer dem i arrayet lines
-            string[] lines = System.IO.File.ReadAllLines(filepath);
-
-            //opretter en liste over class Id'er via GetClassData metoden
-            List<string> ClassIDs = Admin.GetClassData(8);
-
+            string MemberIDClass = "0"; //Starter med 0 for at undgå fejl ved første tilføjelse
+            MemberIDClass = ";" + UserID; //Tilføjer et semicolon før ID'et for at matche formatet i tekstfilen
+            string[] lines = System.IO.File.ReadAllLines(filepath); //Læser alle linjer i tekstfilen Classes.txt og gemmer dem i arrayet lines 
+            List<string> ClassIDs = Admin.GetClassData(8); //Opretter en liste over class Id'er via GetClassData metoden
 
             using (StreamWriter streamWriter = new StreamWriter(filepath)) //har ikke skrevet true - medfører at append er false - vi overskriver filen
             {
-                //opretter int variabel lineNumber
+                //Opretter int variabel lineNumber
                 int lineNumber = 0;
 
-                //loop der itererer gennem listen ClassIDs og tjekker hvornår ClassIDs på index i er lig classID variablen
+                //Loop der itererer gennem listen ClassIDs og ser hvornår ClassIDs på index i er lig classID variablen
                 for (int i = 0; i < ClassIDs.Count; i++)
                 {
-                    //if statement der tjekker hvornår det class id i ClassIDs på index i er lig med classID 
+                    //If statement der tjekker, hvornår det class id i ClassIDs på index i er lig med classID 
                     if (classID.ToString() == ClassIDs[i])
                     {
-                        lineNumber = i; //sætter lineNumber lig med i for at få den linje i lines hvor id'et classID er
+                        lineNumber = i; //Sætter lineNumber lig med i for at få den linje i lines hvor id'et classID er
                     }
                 }
 
-
-                string[] currentClass = lines[lineNumber].Split(";"); //gemmer informationerne om den class på linjen "lineNumber" i et string array. Split kaldes for at splitte stringen op ved hvert 
-                int CurrentJoinAmount = Convert.ToInt32(currentClass[4]); //opretter en int og gemmer værdien på index 4 (som svarer til joinedAmount) i currentClass arrayet. Convert.ToInt32 kaldes for at konverterer fra string til 
-                CurrentJoinAmount++;  //tæller CurrentJoinAmount op med 
-                currentClass[4] = CurrentJoinAmount.ToString();  // tildeler index 4 i currentClass den nye værdi af 
-                string classStringJoined = string.Join(";", currentClass);  //kalder string.Join og kombinerer hvert element i currentClass arrayet med et semicolon og gemmer det i en string
+                string[] currentClass = lines[lineNumber].Split(";"); //Gemmer informationerne om den class på linjen "lineNumber" i et string array. Split kaldes for at splitte stringen op ved hvert ;
+                int CurrentJoinAmount = Convert.ToInt32(currentClass[4]); //Opretter en int og gemmer værdien på index 4 (som svarer til joinedAmount) i currentClass arrayet. Convert.ToInt32 kaldes for at konverterer fra string til int
+                CurrentJoinAmount++;  //Tæller CurrentJoinAmount op med 
+                currentClass[4] = CurrentJoinAmount.ToString();  // Tildeler index 4 i currentClass den nye værdi af 
+                string classStringJoined = string.Join(";", currentClass);  //Kalder string.Join og kombinerer hvert element i currentClass arrayet med et semicolon og gemmer det i en string
                 lines[lineNumber] = classStringJoined; //Erstatter elementet på pladsen "lineNumber" i arrayet lines med den nye string classStringJoined
-                lines[lineNumber] = lines[lineNumber] + MemberIDClass; // tilføjer stringen på pladsen "linenumber" i lines arrayet stringen MemberIDClass
+                lines[lineNumber] = lines[lineNumber] + MemberIDClass; // Tilføjer stringen på pladsen "linenumber" i lines arrayet stringen MemberIDClass
 
-                //for loop der skriver arrayet lines ned i text filen Classes.txt. loopet kører så længe tællervariablen i er mindre end længden på lines arrayet
+                //For loop der skriver arrayet lines ned i text filen Classes.txt. Loopet kører så længe tællervariablen i er mindre end længden på lines arrayet
                 //for hver iteration skrives stringen på index "i" i lines arrayet ned i filen Classes.txt
                 for (int i = 0; i < lines.Length; i++)
                 {
@@ -178,12 +172,12 @@ namespace FørsteSemester
             return memberIDsInClass;
         }
 
-        public void SetMemberIDsInClass(string[] memberIDsInClass) //½
+        public void SetMemberIDsInClass(string[] memberIDsInClass) //
         {
             
-            for (int i = 9; i < memberIDsInClass.Length; i++) //starter fra index 9 for at undgå de første attributter i klassen
+            for (int i = 9; i < memberIDsInClass.Length; i++) //Starter fra index 9 for at undgå de første attributter i klassen
             {
-                //tilføjer hvert medlems ID til memberIDsInClass attributten
+                //Tilføjer hvert medlems ID til memberIDsInClass attributten
                 this.memberIDsInClass = this.memberIDsInClass + memberIDsInClass[i];
 
             }
