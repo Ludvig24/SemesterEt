fa9de3302220ba2d13f73a6a86d9cac18d6b8fb3
Laura Marie Balle Jensen <laje11@student.eadania.dk>
2025-12-10 17:03:49 +0100

    Små Admin kommentar ændringer

--- FørsteSemester/Admin.cs
+++ FørsteSemester/Admin.cs
@@ -11,7 +11,7 @@ namespace FørsteSemester
 {
     internal class Admin : User
     {
-        //Statiske variabler til at finde stien til tekstfilen hvor hold data bliver gemt. kombinere mappen "Documents" med stien til Classes.txt
+        //Statiske variabler til at finde stien til tekstfilen, hvor hold data bliver gemt. Kombinere mappen "Documents" med stien til Classes.txt
         static string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //Finder stien til mappen "Documents" som er ens for alle computere
         static string filepath = Path.Combine(dir, "GitHub\\SemesterEt\\FørsteSemester\\Classes.txt"); //Kombinerer stien til mappen "Documents" med stien til Classes.txt filen
 
@@ -29,18 +29,18 @@ namespace FørsteSemester
             Team.SetRequiredMaxAge(requiredMaxAge);
             Team.SetRequiredMinAge(requiredMinAge);
 
-            //Genererer et unikt hold ID ved at tjekke hvor mange hold der allerede er oprettet og sikre at det nye ID ikke allerede er i brug
-            int TeamID = LoadTeams().Count + 1;//sætter TeamID til at være lig med antallet af hold der allerede er oprettet + 1
+            //Genererer et unikt hold ID ved at tjekke, hvor mange hold der allerede er oprettet og sikre, at det nye ID ikke allerede er i brug
+            int TeamID = LoadTeams().Count + 1;//Sætter TeamID til at være lig med antallet af hold, der allerede er oprettet + 1
             while (true)
             {
-                if (GetClassData(8).Contains(TeamID.ToString())) //tjekker om det genererede TeamID allerede findes i listen af hold ID'er
+                if (GetClassData(8).Contains(TeamID.ToString())) //Tjekker om det genererede TeamID allerede findes i listen af hold ID'er
                 {
-                    TeamID = TeamID + 1; //hvis det gør, øges TeamID med 1 og tjekket gentages
+                    TeamID = TeamID + 1; //Hvis det gør, øges TeamID med 1 og tjekket gentages
                 }
                 else
                 {
-                    Team.SetClassID(TeamID); //hvis det ikke gør, sættes holdets ID til det genererede TeamID
-                    break; //bryder while loopet
+                    Team.SetClassID(TeamID); //Hvis det ikke gør, sættes holdets ID til det genererede TeamID
+                    break; //Bryder while loopet
                 }
 
             }
@@ -51,10 +51,10 @@ namespace FørsteSemester
         //Metode til at få en en specifik type date fra alle hold i en liste baseret på deres indeks (int a)
         public static List<string> GetClassData(int a) 
         {
-            List<string> data = new List<string>(); //opretter en liste til at gemme den ønskede data
-            string dataPoint = ""; //tom string variabel til at holde den ønskede data midlertidigt
-            string[] lines = System.IO.File.ReadAllLines(filepath); //henter data fra tekstfilen i et array
-            for (int i = 0; i < lines.Count(); i++) //looper igennem array og splitter ved hver ;
+            List<string> data = new List<string>(); //Opretter en liste til at gemme den ønskede data
+            string dataPoint = ""; //Tom string variabel til at holde den ønskede data midlertidigt
+            string[] lines = System.IO.File.ReadAllLines(filepath); //Henter data fra tekstfilen i et array
+            for (int i = 0; i < lines.Count(); i++) //Looper igennem array og splitter ved hver ;
             {
                 string ClassData = lines[i]; 
                 string[] ClassSplit = ClassData.Split(";");
@@ -63,7 +63,6 @@ namespace FørsteSemester
             }
 
             return data;
-
         }
 
         //Metoden SaveClass, som gemmer et hold og tiføjer hold objekt til en tekstfil
@@ -71,6 +70,7 @@ namespace FørsteSemester
         {
             using (StreamWriter streamWriter = new StreamWriter(filepath, true))
             {
+                //Skriver attriutterne ned og skiller ved ;
                 streamWriter.Write(team.GetActivity()+ ";");
                 streamWriter.Write(team.GetClassName()+";");
                 streamWriter.Write(team.GetStatus()+";");
@@ -80,6 +80,7 @@ namespace FørsteSemester
                 streamWriter.Write(team.GetRequiredMaxAge()+";");
                 streamWriter.Write(team.GetRequiredMinAge()+";");
                 streamWriter.Write(team.GetClassID());
+                
                 if(team.GetMemberIDsInClass() != null)
                 {
                     streamWriter.Write(team.GetMemberIDsInClass() + ";");
